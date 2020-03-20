9263aad6b56ca470be619c8fc922a7cf7664ce98
AndresQ <a.quintero@dkfz.de>
2020-03-20 04:48:16 +0100

    data explorer link to score filters

--- map_my_corona_shiny/server.R
+++ map_my_corona_shiny/server.R
@@ -702,6 +702,17 @@ function(input, output, session) {
       mutate(Release_Date = as.Date.character(Release_Date)) %>%
       filter(collection_months >= fil_by_collection_date()[1] &
                collection_months <= fil_by_collection_date()[2]) %>% 
+      
+      # Filter by pident
+      filter(pident >= fil_by_score_blastrf_pident()[1] &
+               pident <= fil_by_score_blastrf_pident()[2]) %>% 
+      # Filter by evalue
+      filter(evalue <= fil_by_score_blastrf_evalue()[1] &
+               evalue >= fil_by_score_blastrf_evalue()[2]) %>%
+      # Filter by bitscore
+      filter(bitscore >= fil_by_score_blastrf_bitscore()[1] &
+               bitscore <= fil_by_score_blastrf_bitscore()[2]) %>% 
+      
       select(Accession, pident, evalue, bitscore, Geo_Location, Host,
              Release_Date, Collection_Date, length, mismatch, gapopen,
              qstart, qend, sstart, send, Length, Isolation_Source, Species) %>% 
@@ -725,6 +736,16 @@ function(input, output, session) {
           filter(Geo_Location %in% fil_by_location()) %>% 
           filter(collection_months >= fil_by_collection_date()[1] &
                    collection_months <= fil_by_collection_date()[2]) %>% 
+          # Filter by pident
+          filter(pident >= fil_by_score_blastrf_pident()[1] &
+                   pident <= fil_by_score_blastrf_pident()[2]) %>% 
+          # Filter by evalue
+          filter(evalue <= fil_by_score_blastrf_evalue()[1] &
+                   evalue >= fil_by_score_blastrf_evalue()[2]) %>%
+          # Filter by bitscore
+          filter(bitscore >= fil_by_score_blastrf_bitscore()[1] &
+                   bitscore <= fil_by_score_blastrf_bitscore()[2]) %>% 
+          
           mutate(collection_months = factor(collection_months, 
                                             levels = sort(unique(collection_months)))) %>% 
           ggplot(aes(x = collection_months, fill = collection_months)) +
